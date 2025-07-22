f068d9b4cf8c94a56fbea9fe667e1faab10e5aec
Usuario <hugocanizzabala@gmail.com>
2025-07-21 19:42:03 -0600

    menu fuciones recursivas

---
+++ funciones recursivas.py
@@ -0,0 +1,22 @@
+def menu ()
+    print("menu principal")
+    print("1. factorial de un numero")
+    print("2. suma de dos numeros")
+    print("3. fibonacci de un numero")
+    print("4. cuantas veces aparece una letra en una palabra")
+    print("5. invertir una cadena")
+    print("6. calcular la potencia de un numero")
+    print("7. salir")
+
+
+def factorial(n):
+
+op=int(input("ingrese que opción desea ejecutar"))
+if op==1:
+    num =int(input("ingrese numero"))
+
+elif op ==2:
+
+elif op ==3:
+
+
